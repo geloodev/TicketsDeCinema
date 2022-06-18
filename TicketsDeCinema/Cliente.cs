07330be3271d6db6fa8e2a3f75db27e88711455a
Matheus Germano <dev.mgermano@gmail.com>
2022-06-18 14:50:35 -0300

    User sign in done

--- TicketsDeCinema/Cliente.cs
+++ TicketsDeCinema/Cliente.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,8 @@ namespace TicketsDeCinema
 {
     internal class Cliente
     {
+        Utils utils = new Utils();
+
         private string cpf;
         private string nome;
         private string dataNascimento;
@@ -51,6 +54,44 @@ namespace TicketsDeCinema
 
         public void setSenha(string senha) { this.senha = senha;  }
 
+        public Cliente logIn(string loginCpf, string loginPassword)
+        {
+            Cliente loggedCliente;
 
+            //String de conexão com o banco de dados local
+            string connectionString = @"server=localhost;uid=root;pwd=admin;database=cinema;ConnectionTimeout=2";
+            //cria uma conexão
+            MySqlConnection connection = new MySqlConnection(connectionString);
+
+            connection.Open();
+
+            string base64Password = utils.Base64Encode(loginPassword);
+
+            string queryText = $"select * from cliente where cpf = {loginCpf} and senha = {base64Password}";
+
+            MySqlCommand query = new MySqlCommand(queryText, connection);
+            MySqlDataReader queryData = query.ExecuteReader();
+
+            //caso o comando select tenha retornado alguma linha
+            if (queryData.HasRows)
+            {
+                queryData.Read();
+
+                string cpf = queryData.GetString(0);
+                string nome = queryData.GetString(1);
+                DateTime dataNascimento = queryData.GetDateTime(2);
+                string email = queryData.GetString(3);
+                string senha = queryData.GetString(4);
+
+                loggedCliente = new Cliente(cpf, nome, dataNascimento.ToString(), email, senha);
+            } else
+            {
+                loggedCliente = null;
+            }
+
+            queryData.Close();
+
+            return loggedCliente;
+        }
     }
 }
---
+++ TicketsDeCinema/Utils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketsDeCinema
+{
+    public class Utils
+    {
+        public string Base64Encode(string textToBase64)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(textToBase64);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        public string Base64Decode(string base64EncodedData)
+        {
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
